1746be22dbb2024c597dffa302423b363271c31a
PROAN49 <90123883+PROAN49@users.noreply.github.com>
2022-02-27 14:44:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281368971807'] // Put your number here
+global.owner = ['6285796936422'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -29,7 +29,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'By'
-global.author = ' Vlynn'
+global.author = ' PROAN49'
 global.email = 'venzvl66@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wm = '© Vlynn-Md'
